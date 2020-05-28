89afa889247f783558bd2ac59d0b4cc65eb1ceea
Dave Pernitz <pernitz@uwalumni.com>
2020-05-28 13:52:52 -0600

    Import searchResult for testing

--- src/Components/ResultsPage/ResultsPage.test.tsx
+++ src/Components/ResultsPage/ResultsPage.test.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Header from './Header';
 import ResultsPage from './ResultsPage';
 import { MemoryRouter } from 'react-router-dom'
+import { searchResult } from "../../types"
 
 describe('ResultsPage', () => {
-    
-    
-    interface Result {
-        Name: string
-        Type: string
-        wTeaser: string
-        wUrl: string
-        yUrl: string
-        yID: string
-    }
+
     it('should display some results', () => {
-        const mockResult1: Result = {Name: "An American Tale: Fievel Goes West", Type: "movie", wTeaser: "Best Movie Ever", wUrl: "https://fievel.com", yUrl: "https://youtube.fievel.com", yID: "h3423kdf"}
-        const mockResult2: Result = {Name: "An American Tale", Type: "movie", wTeaser: "Second Best Movie Ever", wUrl: "https://fievel-the-original.com", yUrl: "https://youtube.fievel-the-original.com", yID: "h34kdf"}
+        const mockResult1: searchResult = {Name: "An American Tale: Fievel Goes West", Type: "movie", wTeaser: "Best Movie Ever", wUrl: "https://fievel.com", yUrl: "https://youtube.fievel.com", yID: "h3423kdf"}
+        const mockResult2: searchResult = {Name: "An American Tale", Type: "movie", wTeaser: "Second Best Movie Ever", wUrl: "https://fievel-the-original.com", yUrl: "https://youtube.fievel-the-original.com", yID: "h34kdf"}
 
         const { getByText } = render(<MemoryRouter><ResultsPage results={[mockResult1, mockResult2]}/></MemoryRouter>)
 
