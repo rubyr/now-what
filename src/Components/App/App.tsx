9aff41d9e60fb98256ceed50836a05940be9beb4
December G <dgarnetsmith@gmail.com>
2020-05-28 02:02:56 -0600

    Add fetch call (CORS error)

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,15 +1,49 @@
-import React, { ReactElement } from "react";
+import React, {
+  ReactElement,
+  useEffect,
+  MouseEvent,
+  SyntheticEvent,
+} from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import SearchForm from "../SearchForm/SearchForm";
 
+//state should be empty
+//eventually state will hold the user's search term
+//need another method in this function maybe that will be passed to search form
+
 function App(): ReactElement {
+  interface searchResults {
+    Name: string;
+    Type: string;
+    wTeaser: string;
+    wUrl: string;
+    yUrl: string;
+    yID: string;
+  }
+  const searchTerm = async (searchTerm: string): Promise<searchResults[]> => {
+    //needs to do a fetch call based on the search term and console log results
+    const modifiedSearchTerm: string = searchTerm.split(" ").join("+");
+    const response = await fetch(
+      `https://tastedive.com/api/similar?q=${modifiedSearchTerm}&verbose=1&k=372838-DavePern-7J59GJ8D&limit=5`
+    );
+    const data = await response
+      .json()
+      .then((data) => data)
+      .catch((err) => console.log(err));
+    console.log(data);
+    return data;
+  };
+
+  const click = (term?: string): void => {
+    console.log("did it");
+  };
   return (
     <div className="App">
       <Switch>
         <Route path="/search/:query"></Route>
         <Route exact path="/">
-          <SearchForm />
+          <SearchForm searchTerm={searchTerm} click={click} />
         </Route>
       </Switch>
     </div>
--- src/Components/SearchForm/SearchForm.tsx
+++ src/Components/SearchForm/SearchForm.tsx
@@ -1,17 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, MouseEvent } from "react";
 import "./SearchForm.css";
 
-interface Props {}
+interface searchResults {
+  Name: string,
+  Type: string,
+  wTeaser: string,
+  wUrl: string,
+  yUrl: string,
+  yID: string,
 
-const SearchForm = (props: Props) => {
+}
+
+interface Props {
+  //what goes in here though?
+  //I was going to pass down a function 
+
+  // searchTerm: async (term: string): Promise<searchResults[]> => {
+  //     //needs to do a fetch call based on the search term and console log results 
+  //     const modifiedSearchTerm: string = term.split(' ').join('+')
+  //     const response  = await fetch(`https://tastedive.com/api/similar?q=${modifiedSearchTerm}&verbose=1`);
+  //     const data = await response.json();
+  //     return data;
+  // }
+  searchTerm: (term:string) => Promise<searchResults[]>;
+  click: (term?: string) => void;
+  }
+
+
+const SearchForm: React.FC<Props>= (props: Props) => {
+  console.log(props)
   const [query, setQuery] = useState("");
+  console.log(query)
   return (
-    <form className="SearchForm">
+    <form className="SearchForm" onSubmit={(e) => e.preventDefault()}>
       {/* placeholder is just for tests, you can change it if you like (just update the tests, too) */}
-      <input placeholder="Search for a title" />
-      <button onClick={(e) => e.preventDefault()}>Search</button>
+
+      <input placeholder="Search for a title" onChange={(e) => setQuery(e.target.value)} value={query}/>
+      {/* <button onClick={(query: string, event:MouseEvent<HTMLButtonElement>) => props.searchTerm}>Search</button> */}
+      <button onClick={() => props.searchTerm(query)}>Search</button>
     </form>
   );
 };
 
+//now state is being set using usestate and query is now whatever you type in the box 
+//want to do a fetch request somewhere when you click the button based on what's in state 
 export default SearchForm;
