684269d5fcb4b386abb1768a76a0672d464f56e0
December G <dgarnetsmith@gmail.com>
2020-05-28 18:21:11 -0600

    Begin app testing

--- src/Components/App/App.test.tsx
+++ src/Components/App/App.test.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React, { ReactElement } from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+describe('App', () => {
+    it("should render the App", () => {
+      const { getByText } = render(
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      );
+      const element = getByText("Now What!?");
+      expect(element).toBeInTheDocument();
+    });
+
+//if search button is clicked and there is a search term, should find relevant search items
+//if search button is clicked and there is a search term, should not find relevant search terms where none exist
+
+
+
+})
+
