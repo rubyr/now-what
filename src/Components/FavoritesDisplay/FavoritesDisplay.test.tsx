0d57dd4bfb5209c95f958b0eebcdc0d452067574
Dave Pernitz <pernitz@uwalumni.com>
2020-06-02 07:44:20 -0600

    Add more unit tests

--- src/Components/FavoritesDisplay/FavoritesDisplay.test.tsx
+++ src/Components/FavoritesDisplay/FavoritesDisplay.test.tsx
@@ -6,14 +6,27 @@ import { MemoryRouter } from 'react-router-dom'
 import { searchResult } from "../../types"
 
 describe('ResultsPage', () => {
+    it('should display the heading Favorites', () => {
+        const { getByText } = render(<MemoryRouter><FavoritesDisplay results={[]} toggleFavorite={()=> {}} favorites={[]}/></MemoryRouter>)
+        expect(getByText("Favorites")).toBeInTheDocument()
+    })
 
     it('should display some results', () => {
         const mockResult1: searchResult = {Name: "An American Tale: Fievel Goes West", Type: "movie", wTeaser: "Best Movie Ever", wUrl: "https://fievel.com", yUrl: "https://youtube.fievel.com", yID: "h3423kdf"}
         const mockResult2: searchResult = {Name: "An American Tale", Type: "movie", wTeaser: "Second Best Movie Ever", wUrl: "https://fievel-the-original.com", yUrl: "https://youtube.fievel-the-original.com", yID: "h34kdf"}
-        const mockToggleFavorite = jest.fn()
-        const { getByText } = render(<MemoryRouter><FavoritesDisplay results={[mockResult1, mockResult2]} toggleFavorite={mockToggleFavorite} favorites={[]}/></MemoryRouter>)
+        const { getByText } = render(<MemoryRouter><FavoritesDisplay results={[mockResult1, mockResult2]} toggleFavorite={()=> {}} favorites={[]}/></MemoryRouter>)
 
         expect(getByText("An American Tale: Fievel Goes West")).toBeInTheDocument()
         expect(getByText("An American Tale")).toBeInTheDocument()
+    })
+
+    it('should display results by type', () => {
+        const mockResult1: searchResult = {Name: "An American Tale: Fievel Goes West", Type: "movie", wTeaser: "Best Movie Ever", wUrl: "https://fievel.com", yUrl: "https://youtube.fievel.com", yID: "h3423kdf"}
+        const mockResult2: searchResult = {Name: "The Winter of Our Discontent", Type: "book", wTeaser: "Good read", wUrl: "https://discontent.com", yUrl: "https://youtube.discontent.com", yID: "h34134fu"}
+        const { getByText } = render(<MemoryRouter><FavoritesDisplay results={[mockResult1, mockResult2]} toggleFavorite={() => {}} favorites={[]}/></MemoryRouter>)
+
+        expect(getByText("Movies")).toBeInTheDocument()
+        expect(getByText("Books")).toBeInTheDocument()
+
     })
 })
--- src/Components/Header/Header.test.tsx
+++ src/Components/Header/Header.test.tsx
@@ -6,25 +6,38 @@ import { MemoryRouter } from 'react-router-dom'
 
 describe('Header', () => {
     it('should display the app\'s title', () => {
-        const { getByText } = render(<MemoryRouter><Header /></MemoryRouter>)
+        const { getByText } = render(<MemoryRouter><Header searchTerm={() => {}}/></MemoryRouter>)
 
         expect(getByText('Now What!?')).toBeInTheDocument()
     })
 
     it('should have a search input', () => {
-        const { getByPlaceholderText } = render(<MemoryRouter><Header /></MemoryRouter>)
+        const { getByPlaceholderText } = render(<MemoryRouter><Header searchTerm={() => {}}/></MemoryRouter>)
 
         expect(getByPlaceholderText('search...')).toBeInTheDocument()
     })
 
     it('should display the text as a user enters it', () => {
         
-        const { getByDisplayValue, getByPlaceholderText } = render(<MemoryRouter><Header /></MemoryRouter>)
+        const { getByDisplayValue, getByPlaceholderText } = render(<MemoryRouter><Header searchTerm={() => {}}/></MemoryRouter>)
 
         fireEvent.change(getByPlaceholderText('search...'), { target: {value: 'Pee Wee\'s Big Adventure'}})
 
         expect(getByDisplayValue('Pee Wee\'s Big Adventure')).toBeInTheDocument()
     })
 
-    // test for button/handleclick!
+    it('should call the searchTerm method with the search input when search button is clicked', () => {
+        const mockSearchTerm = jest.fn()
+        const { getAllByText, getByPlaceholderText } = render(<MemoryRouter><Header searchTerm={mockSearchTerm}/></MemoryRouter>)
+
+        fireEvent.change(getByPlaceholderText('search...'), { target: {value: 'Pee Wee\'s Big Adventure'}})
+
+        const searches = getAllByText('Search')
+        const headerSearchButton = searches[0]
+        fireEvent.click(headerSearchButton)
+        expect(mockSearchTerm).toHaveBeenCalledWith('Pee Wee\'s Big Adventure')
+
+
+
+    })
 })
