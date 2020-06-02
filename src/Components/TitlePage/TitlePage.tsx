4d93d748fadf66f4626422268bb510c47df313f9
December G <dgarnetsmith@gmail.com>
2020-06-02 02:14:06 -0600

    Use fetch call to retrieve information about a specific title

--- src/Components/TitlePage/TitlePage.tsx
+++ src/Components/TitlePage/TitlePage.tsx
@@ -2,40 +2,131 @@ import React, { useState, useEffect } from "react";
 import { searchResult } from "../../types";
 import "./TitlePage.css";
 // import youtube from '../../../public/images/youtube_logo.png'
+import { findTitleInfo } from "../../apiCalls";
+import { setConstantValue } from "typescript";
 
 interface Props {
-  item: searchResult;
+  // item?: searchResult;
+  url: string;
 }
 
+//I'm trying to make a fetch call
+//based on the url, I want to make a fetch call to that endpoint of the api
+//it should then display the appropriate information on the page
+//the title page is being run twice
+//my fetch call was being run twice, too
+//I don't know how to use useeffect effectively
+
+//why is the fetch call sending so many requests? It sends at least two no matter what I do.
+//I don't get why I can't seem to add the results of the fetch call to state
+//should I just save it in a variable?
+// const FavoritesList = (props: Props) => {
+//   const [results, setResults] = useState<searchResult[]>([]);
+
+//   useEffect(() => {
+//     async function fetchData() {
+//       const { Similar } = await fetchFavorites(props.favorites);
+
+//       if (Similar) setResults(Similar.Info);
+//     }
+//     fetchData();
+//   }, [props.favorites]);
+
+//normally I would do this in a separate file but handle in component did mount, I think
+//then I would pass down the information as a prop
+//I would invoke the function on page load?
+//that doesn't make sense
+
 const TitlePage: React.FC<Props> = (props: Props) => {
+  // debugger;
   // const actualTitle: string = props.name.split('+').join(' ')
-  // const [title, setTitle] = useState(actualTitle);
+  const [results, setResults] = useState<searchResult[]>([]); // const [info, setInfo] = useState([]);
+  const [info, setInfo] = useState<searchResult[]>([]); // const [info, setInfo] = useState([]);
   //don't need to set title
   //do I really need to set state?
   // setTitle(props.name.split('+').join(' '))
-  return (
-    <section className="title-page">
-      <aside className="title-overview">
-        <h4>{props.item.Name}</h4>
-        <figure className="result-card-image-placeholder"></figure>
-        <a href={props.item.yUrl} target="_blank" rel="noopener noreferrer">
+  console.log("happening");
+  //do fetch call here?
+  //props.name only exists...when?
+  //props.name exists if you are not coming from the search results
+  console.log(props.url);
+  // console.log(title)
+
+  // let results: {Results: object}[] = [];
+  // let info: {Info: object}[] = [];
+  useEffect(() => {
+    async function retrieveInfo() {
+      const allData = await findTitleInfo(props.url)
+        // .then((response) => (response.ok ? response.json() : null))
+        .then((data) => {
+          if (data) {
+            // results.push(data.Similar.Results);
+            // info.push(...data.Similar.Info)
+            setResults(data.Similar.Results);
+            setInfo(data.Similar.Info);
+            return data;
+            // console.log(data.Similar.Results)
+          }
+        });
+    }
+    retrieveInfo();
+    return setResults([]);
+  }, [props.url]);
+
+  // console.log(results);
+  // console.log(info[0].Name)
+
+  // .then((data) => console.log(results));
+
+  // console.log(randomArray)
+  // .then((info) => setResults(info));
+  // .then(response => setTitle({...response.Similar}))
+  // .then((data) => console.log(results));
+  //why is this happening twice?
+  // return (
+  //   <section>
+  //     <h4>Hi</h4>
+  //   </section>
+  // );
+  if (!results.length || !info.length) {
+    return (
+      <section>
+        <h5>Loading...</h5>
+      </section>
+    );
+  } else {
+    return (
+      <section className="title-page">
+        <aside className="title-overview">
+          <h4>{info[0].Name}</h4>
+          <figure className="result-card-image-placeholder"></figure>
+          <a
+            href={info[0].yUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
           <img
             className="youtube-link"
             src="/images/youtube_logo.png"
             alt="youtube-logo"
           ></img>
-        </a>
-      </aside>
-      <main className="title-detailed-info">
-        <p>{props.item.wTeaser}</p>
-        <p className="wiki-link">
-          <a href={props.item.wUrl} target="_blank" rel="noopener noreferrer">
-            Read more...
           </a>
-        </p>
-      </main>
-    </section>
-  );
+        </aside>
+        <main className="title-detailed-info">
+          <p>{info[0].wTeaser}</p>
+          <p className="wiki-link">
+            <a
+              href={info[0].wUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+            Read more...
+            </a>
+          </p>
+        </main>
+      </section>
+    );
+  }
 };
 
 export default TitlePage;
