051ec845ae8903619bdd7ebfa2870cddf91f0a42
December G <dgarnetsmith@gmail.com>
2020-06-03 14:36:56 -0600

    Update styling of Header and SearchForm components

--- src/Components/SearchForm/SearchForm.tsx
+++ src/Components/SearchForm/SearchForm.tsx
@@ -16,16 +16,29 @@ const SearchForm: React.FC<Props> = (props: Props) => {
     }
   };
   return (
-    <form className="SearchForm" onSubmit={(e) => e.preventDefault()}>
-      <input
-        placeholder="Search for a title"
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyDown={(e) => e.key === "Enter" && search()}
-        value={query}
-        aria-label="Title Search"
-      />
-      <button onClick={search}>Search</button>
-    </form>
+    <section className="form-background">
+      {/* <img
+        src="images/mainpage-background.jpg"
+        className="form-background-image"
+      >
+      </img> */}
+      <section className="text-container">
+
+      <h1 className="main-page-text">Hi</h1>
+      </section>
+      <section className="form-container">
+        <form className="SearchForm" onSubmit={(e) => e.preventDefault()}>
+          <input
+            placeholder="Search for a title"
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyDown={(e) => e.key === "Enter" && search()}
+            value={query}
+            aria-label="Title Search"
+          />
+          <button onClick={search}>Search</button>
+        </form>
+      </section>
+    </section>
   );
 };
 
