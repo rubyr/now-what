0b0ef705f6ff0e3de410ac5e098be09a356d8d61
rubyr <rubyrinken@gmail.com>
2020-09-21 15:02:55 -0600

    Updates

---
+++ precache-manifest.4238cdb73b68a2640e0830a16337fd4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca3ca8e59b3355694323ac57740e99b",
+    "url": "/now-what/index.html"
+  },
+  {
+    "revision": "2d17209c021451536dff",
+    "url": "/now-what/static/css/main.bf2654cc.chunk.css"
+  },
+  {
+    "revision": "17deb838b59cf876b0e8",
+    "url": "/now-what/static/js/2.755b2a11.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/now-what/static/js/2.755b2a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d17209c021451536dff",
+    "url": "/now-what/static/js/main.f3e97ce7.chunk.js"
+  },
+  {
+    "revision": "e8756c7c2c69f292eac9",
+    "url": "/now-what/static/js/runtime-main.bf63eaec.js"
+  }
+]);
